8bc755df496aae5112c4e67d179594e47a6c4254
juliancolman <juliancolman12@gmail.com>
2024-07-04 19:45:02 -0300

    updt

--- src/components/Appointments/Doctor/CreateAppointment/CreateAppointment.tsx
+++ src/components/Appointments/Doctor/CreateAppointment/CreateAppointment.tsx
@@ -6,9 +6,12 @@ import { TAppointmentDate } from '../../../../models/types/requests/TAppointment
 import AppointmentService from '../../../../services/AppointmentService';
 import { useAuth } from '../../../../contexts/UserContext/AuthContext';
 import './CreateAppointment.css'; // Importa el archivo CSS que contiene los estilos
+import ScreenMessage from '../../../ScreenMessage/ScreenMessage';
 
 const CreateAppointment = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [message, setMessage] = useState<string>('');
+  const [status, setStatus] = useState<number | null>(null);
   const { user } = useAuth();
   const navigate = useNavigate();
 
@@ -26,13 +29,21 @@ const CreateAppointment = () => {
       try {
         console.log(appointmentData);
         await AppointmentService.createAppointment20Min(appointmentData);
-        alert('Turnos agregados correctamente');
-        navigate('/AppointmentListDoctor');
+        setMessage('Turnos agregados correctamente');
+        setStatus(200); // Por ejemplo, usar 200 para indicar éxito
+        // Aquí podrías agregar lógica adicional si necesitas redirigir automáticamente
       } catch (error) {
         console.error('Error al crear el turno:', error);
-        alert('Error al crear el turno. Por favor, inténtelo de nuevo.');
+        setMessage('Error al crear el turno. Por favor, inténtelo de nuevo.');
+        setStatus(400); // Por ejemplo, usar 400 para indicar error
       }
     }
+  };
+
+  const handleScreenMessageClose = () => {
+    setMessage('');
+    setStatus(null);
+    navigate('/AppointmentListDoctor'); // Redirige a la lista de turnos del doctor después de cerrar el mensaje
   };
 
   return (
@@ -50,6 +61,14 @@ const CreateAppointment = () => {
         <p className='ejemplo'>Por ejemplo: si usted trabaja de 10:00 a 12:00, se le cargarían 7 turnos en la fecha seleccionada.</p>
       </div>
       <button onClick={handleSaveAppointment}>Guardar Turnos</button>
+
+      {message && (
+        <ScreenMessage
+          message={message}
+          status={status || 500} // Usar 500 como estado por defecto si no se especifica
+          onClose={handleScreenMessageClose}
+        />
+      )}
     </div>
   );
 };
