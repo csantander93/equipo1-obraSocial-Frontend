049ee74b0b49dc93eda032da2404065f729bab7f
Cristian Santander <casdesarrollo.2022@gmail.com>
2024-07-06 21:22:43 -0300

    update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const FooterPag: React.FC = () => {
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; {new Date().getFullYear()} AlMedin. Todos los derechos reservados.</p>
-        <p>hecho con amor ❤️ por</p>
+        <p>Hecho con amor ❤️ por</p>
         <ul className="footer-links">
           <li><a href="https://www.linkedin.com/in/cristian-santander-03807b17b/" target="_blank" rel="noopener noreferrer">Cristian Santander</a></li>
           <li>y</li>
