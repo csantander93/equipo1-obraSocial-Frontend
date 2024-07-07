17b0024757002b3110a259d43fd81653d81df213
Cristian Santander <casdesarrollo.2022@gmail.com>
2024-07-06 21:22:09 -0300

    footer linkedin santander colman se abrn en nueva pestaña

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,9 +7,9 @@ const FooterPag: React.FC = () => {
         <p>&copy; {new Date().getFullYear()} AlMedin. Todos los derechos reservados.</p>
         <p>hecho con amor ❤️ por</p>
         <ul className="footer-links">
-          <li><a href="https://www.linkedin.com/in/cristian-santander-03807b17b/">Cristian Santander</a></li>
+          <li><a href="https://www.linkedin.com/in/cristian-santander-03807b17b/" target="_blank" rel="noopener noreferrer">Cristian Santander</a></li>
           <li>y</li>
-          <li><a href="https://www.linkedin.com/in/juli%C3%A1n-colman-7a32b6267/">Julián Colman</a></li>
+          <li><a href="https://www.linkedin.com/in/juli%C3%A1n-colman-7a32b6267/" target="_blank" rel="noopener noreferrer">Julián Colman</a></li>
         </ul>
       </div>
     </footer>
